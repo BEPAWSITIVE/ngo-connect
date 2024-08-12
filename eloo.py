0f6e7a4b9e75fc37f1d6f6578fc3b60d2ad58e1b
DEEPAK MISHRA <DM5563203@GMAIL.COM>
2024-08-12 16:51:18 +0530

    Add files via upload

---
+++ eloo.py
@@ -0,0 +1,13 @@
+print("hello")
+a = 2
+print(a)
+print(x)
+q = int(input("Enter a no = "))
+print ("The value of q is = ")
+p = int(input("Enter a no = "))
+print("The value of p is = ",p)
+
+a = int(input("Enter a number = "))
+b = int(input("Enter another number = "))
+c = a+b
+print("The addition of a and b is",c)
